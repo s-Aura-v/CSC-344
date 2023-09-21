3119ff2500f4d5d0a3cfdcf28ce8147530d552b6
s-Aura-v <slamich2@oswego.edu>
2023-09-20 22:40:59 -0400

    Created a doubly linked list

--- MacroProjectOne.c
+++ MacroProjectOne.c
@@ -5,28 +5,57 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct Tape {
-    char *initial;
+struct TapeCell {
+    char data;
     struct TapeCell* prev;
     struct TapeCell* next;
 };
 
 struct Instruction {
-    int currentState;
-    char readValue;
-    char writeValue;
-    char moveDirection; // 'L' for left, 'R' for right
-    int newState;
+     int currentState;
+     char readValue;
+     char writeValue;
+     char moveDirection; // 'L' for left, 'R' for right
+     int newState;
 };
 
+//Global variables
+struct TapeCell* head = NULL;
+
+//Functions
+struct TapeCell* createNewCell(char charValue);
+void addNext(char charValue);
+
+
 int main() {
-    //Read the input file
-    FILE *fp;
-    fp = fopen("input_test_1.txt", "r"); //r = file is created for reading
-
-
-
+    //Initialize the head
+    head = createNewCell('A');
     
-    fclose(fp);
+
+
+    //End of program
     return 0;
 }
+
+
+
+struct TapeCell* createNewCell(char charValue) {
+    struct TapeCell* newCell = (struct TapeCell*) malloc(sizeof (struct TapeCell));
+    newCell->data = charValue;
+    newCell->prev = NULL;
+    newCell->next = NULL;
+    return newCell;
+}
+
+void addNext(char charValue) {
+    //Create a temporary variable so we don't edit the main head
+    struct TapeCell* temp = head;
+    struct TapeCell* newCell = createNewCell(charValue);
+    while (temp->next != NULL) {
+        temp = temp->next;
+    }
+    temp->next = newCell;
+    newCell->prev = temp;
+}
+
+
