123d2ccb36211a9f8135cf8b7f5e5a730d18e98c
s-Aura-v <slamich2@oswego.edu>
2023-09-21 00:52:12 -0400

    Lines added to variables

--- MacroProjectOne.c
+++ MacroProjectOne.c
@@ -4,6 +4,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
+
+#define MAX_LINE 2048
+
 
 struct TapeCell {
     char data;
@@ -30,13 +34,78 @@ void addNext(char charValue);
 int main() {
     //Initialize the head
     head = createNewCell('A');
+
+    //Read for file name
+    char fileName[20];
+    printf("Input the name of the file: ");
+    scanf("%s", fileName);
+
+    //Read the file and add it into an array
+    FILE *fp;
+    fp = fopen(fileName, "r"); //r = file is created for reading
+    char *line;
+    line = malloc(sizeof (char * ) * 100);
+    (fgets(line, sizeof(line), fp) != NULL);      // f(gets): store, size, inputFile
+
+//    Create the tape
+    for (int i = 0; i < strlen(line); i++) {
+        printf("%c", line[i]);
+        addNext(line[i]);
+    }
+    for (int i = 0; i < 9; i++) {
+        printf("%c", head[i].data);
+    }
+
+    //Instruction Setup
+    int numOfStates; //initialize it based on input
+    int startState;
+    int endState;
+    char numTest[100];
+
+
+    //Go through the files lines, set each variable to proper line
+    char buffer[MAX_LINE];
+    bool keepReading = true;
+    int currentLine = 0;
+
+    //Insert value to variables
+    do
+    {
+        fgets(buffer, 20, fp); //add the text to, max line for array, file
+        if (feof(fp)) {
+            keepReading = false;
+        }
+        else if (currentLine == 1) {
+            numOfStates = atoi(buffer); //cpy buffer to
+        }
+        else if (currentLine == 2) {
+            startState = atoi(buffer);
+        }
+        else if (currentLine == 3) {
+            endState = atoi(buffer);
+        }
+        currentLine++;
+    } while (keepReading);
+
+
+    printf("%d %d %d ",numOfStates, startState, endState);
+
+    fclose(fp);
+
     
+    //Create the 2d array of instructions
+    struct Instruction** instructionTable = (struct Instruction***)malloc(numOfStates * sizeof(struct Instruction**));
+    for (int i = 0; i < numOfStates; i++) {
+        instructionTable[i] = malloc(128 * sizeof(struct Instruction *));
+    }
+
+
 
 
     //End of program
     return 0;
-}
 
+}
 
 
 struct TapeCell* createNewCell(char charValue) {
@@ -48,7 +117,7 @@ struct TapeCell* createNewCell(char charValue) {
 }
 
 void addNext(char charValue) {
-    //Create a temporary variable so we don't edit the main head
+    //Create a temporary variable
     struct TapeCell* temp = head;
     struct TapeCell* newCell = createNewCell(charValue);
     while (temp->next != NULL) {
