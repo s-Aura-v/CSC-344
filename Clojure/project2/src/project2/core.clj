7e41d4bd53f9c3700bc397742e55de942f0fc14d
s-Aura-v <slamich2@oswego.edu>
2023-09-26 15:25:25 -0400

    Not-elimination finally works

--- Clojure/project2/src/project2/core.clj
+++ Clojure/project2/src/project2/core.clj
@@ -10,22 +10,59 @@
 ;(defn isItNot?
 ;  "Determine if value is not"
 ;  )
-(defn not-count
-  "Count how many times a word appears"
-  [string]
-  (count (re-seq #"not" string)))
+
+;;Tests
+(def string '(not (not (not (x)))))
+(first string)
+(last string)
+(next string)
+(first (next string))
+(reduce 'not string)
+(first (reduce 'not string))
+(reduce 'not string)
 
 (defn not-elimination
-  "Take a function and clarify its double negatives"
-  [not-expr]
-  (if (str/includes? [not-expr] "not" )
-       "Has not"
-       "Not has not"
+  "Eliminate double-nots from a function of nots "
+  [expr]
+  (if (and (= 'not (first expr)) (= 'not (first (reduce 'not expr))))
+    ;;if there are more than two not expressions
+    (reduce 'not (reduce 'not expr))
+    ;; Add to true an expression that results in the values after the first string
+    ()
+    ;; Add to false an null expression
     ))
 
-(not-elimination "(not (not x))")
-(not-count "(not (not x))")
-(not-count `(not (not x)))
+(defn not-elimination2
+  "Eliminate double-nots from a function of nots "
+  [expr]
+  (if (and (= 'not (first string)) (= 'not (first (reduce 'not string))))
+    ;;if there are more than two not expressions
+    (reduce 'not (reduce 'not string))
+    ;; Add to true an expression that results in the values after the first string
+    {}
+    ;; Add to false an null expression
+  ))
+
+(defn not-elimination3
+  "Eliminate double-nots from a function of nots"
+  [expr]
+  (if
+    (and (= 'not (first expr)) (= 'not (first (second expr))))
+    ;; If there are two consecutive 'not' expressions, return the inner expression.
+    (second (second expr))
+
+    ()
+    ;; If not, return the original expression.
+    expr))
+
+
+;; Tests
+(not-elimination string)
+(not-elimination '(not x))
+(not-elimination '(a))
+(not-elimination3 '(not (not a)))
+(not-elimination3 '(not (not (and a b))))
+(not-elimination3 '(not (not (not (not c)))))
 
 
 (if (str/includes? "(not x)" "not" )
