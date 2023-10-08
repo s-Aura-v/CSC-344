47500bae0d29d7f7aadd8bd7649f923043ca4fb4
s-Aura-v <slamich2@oswego.edu>
2023-10-07 20:22:19 -0400

    Output = Sets
    Converted all Lists to sets

--- Clojure/project2/src/project2/core.clj
+++ Clojure/project2/src/project2/core.clj
@@ -11,13 +11,13 @@
     (and (= 'not (first expr))
          (list? (second expr)) (= 'not (first (second expr))))
     ;; check if there are 2 nots
-    (second (second expr))
+    #{(second (second expr))}
     ;; if not, check if there is not is present at all
     (if (= 'not (first expr))
       ;;return nothing if there is a not
-      ()
+      #{()}
       ;;return the expression if there are no nots
-      expr)
+      #{expr})
     ))
 
 ;; Tests
@@ -49,11 +49,11 @@
   [if-prop]
   (if  (= (second (first if-prop)) (nth if-prop 2))
     ;;if first input is equal to given variable
-    (second (first if-prop))
+    #{(second (first if-prop))}
     ;;else if second input is equal to given variable
     (if (= (nth (first if-prop) 2) (nth if-prop 2))
-      (nth (first if-prop) 2)
-      ()
+      #{(nth (first if-prop) 2)}
+      #{()}
      )))
 
 ;;Tests
@@ -68,9 +68,9 @@
   "Evaluate (not A) from if (A B) and (not B)"
   [if-prop]
   (if  (= (second (first if-prop)) (last (nth if-prop 2)))
-    (list 'not (nth (first if-prop) 2))
+    (set ['not (nth (first if-prop) 2)])
     ;;Remember to check if its even there
-    (list 'not (second (first if-prop)))
+    (set ['not (second (first if-prop))])
     )
   )
 
@@ -92,17 +92,38 @@
       (and-elimination prop)
       ;;if X Y and X infer Y
       (if (and (list? prop) (= 'if (first prop)) (list? (last prop)) ())
+        ;; if it's a list, do tollens
+        (modus-tollens prop)
+        ;;if not, do ponens
         (modus-ponens prop)
-        ;; if X Y and not X, infer X
-        (if (and (list? prop) (= 'if (first prop)) (list? (last prop)) ())
-          (modus-tollens prop)
-          prop
-          )
         )
       )
     )
   )
 
-(def testF '(and (not (not if (a b)))  a))
+;;Tests
+(def testF '(and (not (not if (a b))) a))
 (elim-step testF)
+
+;;Infer-fwd
+
+(defn fwd-infer
+  "Make logical inferences based on propositions"
+  [prop known]
+  (loop [ prop known]
+    if (empty? prop)
+    (concat prop )
+  )
+
+
+
+(empty? '(1 2 3))
+(empty? ())
+
+
+;;Tests
+(fwd-infer '(if a b) '#{(not b)})
+(fwd-infer 'a '#{(if a b) (if b c)})
+(fwd-infer-elim '(and (not (not (if a b))) a) '#{})
+
 
