03393e13a348f55dc027494b3e6bf15ff32a5adb
s-Aura-v <slamich2@oswego.edu>
2023-10-24 20:34:06 -0400

    Skeleton for oCaml Project

--- ocaml_proj3/mainproj3.ml
+++ ocaml_proj3/mainproj3.ml
@@ -1,8 +1,16 @@
 (*
 S -: E$  
 E -: C | EE | E'|'E | E'?' | '(' E ')'
-C -: '0' | '1' | ... | '9'| 'a' | 'b' | ... | 'z' | '.
-'*)
+C -: '0' | '1' | ... | '9'| 'a' | 'b' | ... | 'z' | '.'
+
+ S  -: E$
+ E  -: T '|' E | T
+ T  -: F T | F
+ F  -: A '?' | A
+ A  -: C | '(' E ')'
+ C  -: Alphanumeric characters plus '.'
+*)
+
 
 #directory "+str"
 #load "str.cma"
@@ -17,13 +25,13 @@ type token =
   | Tok_RPAREN
   | Tok_END
 
-  let re_alphabet = Str.regexp "([A-Za-z0-9]+)"
-  let re_or = Str.regexp "|"
-  let re_q = Str.regexp "?"
-  let re_lparen = Str.regexp "()"
-  let re_rparen = Str.regexp ")"
-
-  exception IllegalExpression of string
+let re_alphabet = Str.regexp "[A-Za-z0-9]+"  (* Make the regex case-insensitive *)
+let re_or = Str.regexp "|"
+let re_q = Str.regexp "?"
+let re_lparen = Str.regexp "("
+let re_rparen = Str.regexp ")"
+(* Define a custom exception for parsing errors *)
+exception ParseError of string
 
 (* Tokenize the input regular expression string *)
 let tokenize str =
@@ -42,12 +50,9 @@ let tokenize str =
     else if (Str.string_match re_rparen s pos) then
       Tok_RPAREN :: (tok (pos + 1) s)
     else
-      raise (IllegalExpression "tokenize")
+      raise (ParseError "tokenize")
   in
   tok 0 str
-
-
-(* ... (Parser) ... *)
 
 (* AST Types *)
 type re =
@@ -58,80 +63,5 @@ type re =
 
 let tok_list = ref []
 
-exception ParseError of string
-
-let lookahead () =
-  match !tok_list with
-    [] -> raise (ParseError "no tokens")
-  | (h::t) -> h
-
-let rec parse_OR () =
-  let t = lookahead () in
-  match t with
-  | Tok_Char _ | Tok_LPAREN ->
-    let e1 = parse_LParen () in
-    let t' = lookahead () in
-    (match t' with
-     | Tok_OR ->
-       match_tok Tok_OR;
-       let e2 = parse_OR () in
-       Alternation (e1, e2)
-     | _ -> e1)
-  | _ -> raise (ParseError "parse_OR")
-
-and parse_LParen () =
-  let f1 = parse_Q () in
-  let t' = lookahead () in
-  match t' with
-  | Tok_Char _ | Tok_LPAREN ->
-    let f2 = parse_Q () in
-    Concat (f1, f2)
-  | _ -> f1
-
-and parse_Q () =
-  let a = parse_A () in
-  let t' = lookahead () in
-  match t' with
-  | Tok_Q ->
-    match_tok Tok_Q;
-    Optional a
-  | _ -> a
-
-and parse_A () =
-  let t = lookahead () in
-  match t with
-  | Tok_Char c ->
-    match_tok (Tok_Char c);
-    C c
-  | Tok_LPAREN ->
-    match_tok Tok_LPAREN;
-    let e = parse_E () in
-    match_tok Tok_RPAREN;
-    e
-  | _ -> raise (ParseError "parse_A")
 
 
-
-(*Interpreter*)
-let parse_pattern pattern =
-  tok_list := tokenize pattern;
-  parse_E ()
-
-let pattern_to_string pattern =
-  let rec pattern_to_string_helper pattern =
-    match pattern with
-    | C c -> String.make 1 c
-    | Concat (p1, p2) ->
-      let str1 = pattern_to_string_helper p1 in
-      let str2 = pattern_to_string_helper p2 in
-      str1 ^ str2
-    | Optional p -> "(" ^ pattern_to_string_helper p ^ ")?"
-    | Alternation (p1, p2) ->
-      let str1 = pattern_to_string_helper p1 in
-      let str2 = pattern_to_string_helper p2 in
-      "(" ^ str1 ^ "|" ^ str2 ^ ")"
-  in
-  pattern_to_string_helper pattern
-
-
-(* Example usage *)
